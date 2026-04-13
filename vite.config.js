c7302e5b3349804f710671abb94289f0faa12c2a
Rachel <rachelaurellyahya@gmail.com>
2026-04-13 14:38:17 +0700

    debugging

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/home.js',
-                'resources/js/catalog.js'
-            ],
-            refresh: true,
-        }),
-        vue(),
-        
-    ],
-    base: '/',
-    build:{
-        outDir: 'dist'
-    }
+  plugins: [vue()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
